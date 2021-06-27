913bca72b91d738386690f8733eecb585251e827
Joan <juanbass84@gmail.com>
2021-06-27 20:32:48 +0200

    falta revisar doctest2

---
+++ avrexcept.py
@@ -0,0 +1,15 @@
+class AVRException(object):
+    def __init__(self):
+        self.BreakException=BreakException
+        self.UnknownException=UnknownOpcodeException
+        self.OutOfMemException=OutOfMemException
+    
+class BreakException(AVRException):
+    pass
+
+class UnknownOpcodeException(AVRException):
+    pass
+
+class OutOfMemException(AVRException):
+    pass
+
